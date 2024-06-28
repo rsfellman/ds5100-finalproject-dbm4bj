28aa58a85e0a8f61c709f0e9bb7ad67a70a1d8d4
Rachel Fellman <rachel.s.fellman@gmail.com>
2024-06-28 10:46:27 -0400

    created class outlines

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
 
+class Die:
+    def __init__:
+        pass
+
+class Game:
+    def __init__:
+        pass
+
+class Analyzer:
+    def __init__:
+        pass
+    
