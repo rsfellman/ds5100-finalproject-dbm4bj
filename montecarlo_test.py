0be514272277d266d118601c1e247a2bab9f6ded
Rachel Fellman <rachel.s.fellman@gmail.com>
2024-07-06 15:40:16 -0400

    fixed typos

--- montecarlo_test.py
+++ montecarlo_test.py
@@ -7,18 +7,17 @@ import unittest
 
 class DieTestSuite(unittest.TestCase):
     '''
-    This module tests the methods in the die class unsing unittest.
+    TTests the methods in the Die class unsing unittest.
 
     Methods:
-    test_01_initializer: This tests if a numpy array of the faces is created/stored
-    test_02_change_weight: checks to see if the new weight is stored in the data frame with the correct side.
-    test_03_roll_die:Tests if the results are a list containing faces of the die.
-        This is done using the all method to see if the results are in our originial list of die faces and isinstance to make sure it is a list. 
-    test_04_get_current_state: This tests if the outcome of this method is a dataframe with a column for wieghts.
+    test_01_initializer: Tests if a numpy array of the faces is created/stored
+    test_02_change_weight: Checks to see if the new weight is stored in the data frame with the correct side.
+    test_03_roll_die: Tests if the results are a list containing faces of the die.
+    test_04_get_current_state: Tests if the outcome of this method is a dataframe with a column for wieghts.
     '''
     def test_01_initializer(self):
         '''
-        This tests if a numpy array of the faces is created/stored
+        Tests if a numpy array of the faces is created/stored from an instance of the Die class.
         '''
         die1 = [1,2,3,4,5,6]
         die_arr = np.array(die1)
@@ -27,7 +26,7 @@ class DieTestSuite(unittest.TestCase):
 
     def test_02_change_weight(self):
         '''
-        checks to see if the new weight is stored in the data frame with the correct side.
+        Checks to see if the new weight is stored in the data frame with the correct side.
         '''
         die1 = [1,2,3,4,5,6]
         die_arr = np.array(die1)
@@ -51,7 +50,7 @@ class DieTestSuite(unittest.TestCase):
 
     def test_04_get_current_state(self):
         '''
-        This tests if the outcome of this method is a dataframe with a column for wieghts.
+        Tests if the outcome of this method is a dataframe with a column for weights.
         '''
         die1 = [1,2,3,4,5,6]
         die_arr = np.array(die1)
@@ -62,16 +61,16 @@ class DieTestSuite(unittest.TestCase):
 
 class GameTestSuite(unittest.TestCase):
     '''
-    This tests to make sure the Game class is functioning as expected.
+    Tests to make sure the Game class is functioning as expected.
     ---
-    methods:
-    test_05_initializer: This tests to make sure our initializer creates and attribute which is equal to out input for an instance of the Game class.
-    test_06_play: This tests to see if the play method correctly sampled the dice for a given number of rolls.
-    test_07_show_last_play: This tests to make sure the output of the show_last_play method is a dataframe.
+    Methods:
+    test_05_initializer: Tests to make sure the initializer creates and attribute which is equal to out input for an instance of the Game class.
+    test_06_play: Tests to see if the play method correctly sampled the dice for a given number of rolls.
+    test_07_show_last_play: Tests to make sure the output of the show_last_play method is a dataframe.
     '''
     def test_05_initializer(self):
         '''
-        This tests to make sure our initializer creates and attribute which is equal to out input for an instance of the Game class.
+        Tests to make sure our initializer creates and attribute which is equal to out input for an instance of the Game class.
         '''
         dice = [1,2,3,4]
         dice_arr = np.array(dice)
@@ -89,7 +88,7 @@ class GameTestSuite(unittest.TestCase):
 
     def test_06_play(self):
         '''
-        this tests to see if the play method correctly sampled the dice for a given number of rolls.
+        Tests to see if the play method correctly sampled the dice for a given number of rolls.
         '''
         dice = [1,2,3,4]
         dice_arr = np.array(dice)
@@ -109,7 +108,7 @@ class GameTestSuite(unittest.TestCase):
     
     def test_07_show_last_play(self):
         '''
-        This tests to make sure the output of the show_last_play method is a dataframe.
+        Tests to make sure the output of the show_last_play method is a dataframe.
         '''
         dice = [1,2,3,4]
         dice_arr = np.array(dice)
@@ -131,20 +130,20 @@ class GameTestSuite(unittest.TestCase):
 
 class AnalyzerTestSuite(unittest.TestCase):
     '''
-    The AnalyzerTestSuite has a series of unittests to make sure the Analyzer class from the montecarlo simulation is functioning as expected.
+    A series of unittests to make sure the Analyzer class is functioning as expected.
     ---
-    methods:
-    test_08_initializer: This tests that the attribute data created is a dataframe.
-    test_09_count_jackpots: This tests that the number of jackpots is counted correctly and returned as an integer.
-    test_10_count_faces: This tests if the count_faces method correctly counts the number of faces.
-    test_11_count_combos: This tests to see if the output of the count_combos method is a dataframe with a MultiIndex by calculating the numbers of levels in the index.
-    test_12_count_permutations: This tests the the output of the count_permutations method is a dataframe with a MultiIndex.
+    Methods:
+    test_08_initializer: Tests that the attribute "data" created is a dataframe.
+    test_09_count_jackpots: Tests that the number of jackpots is counted correctly and returned as an integer.
+    test_10_count_faces: Tests if the count_faces method correctly counts the number of faces.
+    test_11_count_combos: Tests to see if the output of the count_combos method is a dataframe with a MultiIndex.
+    test_12_count_permutations: Tests the the output of the count_permutations method is a dataframe with a MultiIndex.
 
     
     '''
     def test_08_initializer(self):
         '''
-        this tests that the attribute data created is a dataframe.
+        Tests that the attribute data created is a dataframe.
         '''
         dice = [1,2,3,4]
         dice_arr = np.array(dice)
@@ -166,9 +165,9 @@ class AnalyzerTestSuite(unittest.TestCase):
 
     def test_09_count_jackpots(self):
         '''
-        This tests that the number of jackpots is counted correctly and returned as an integer.
+        Tests that the number of jackpots is counted correctly and returned as an integer.
         This test will set every die to have one face so every roll should be a jackpot.
-        For the purpose of this test we ignore the fact that it is imposible to have a one sided die unless it is a mobius strip of some sort.
+        For the purpose of this test, we ignore the fact that it is imposible to have a one sided die unless it is a mobius strip of some sort.
         '''
         dice = [1]
         dice_arr = np.array(dice)
@@ -190,7 +189,7 @@ class AnalyzerTestSuite(unittest.TestCase):
 
     def test_10_count_faces(self):
         '''
-        This tests if the count_faces method correctly counts the number of faces.
+        Tests if the count_faces method correctly counts the number of faces.
         Ignoring reality once again, we will have one sided dice, so the face count for each roll should be equal to the number of dice rolled, in this case 4.
         This test will also confirm that the output is a dataframe.
         '''
@@ -215,7 +214,7 @@ class AnalyzerTestSuite(unittest.TestCase):
 
     def test_ll_count_combos(self):
         '''
-        This tests to see if the output of the count_combos method is a dataframe with a MultiIndex by calculating the numbers of levels in the index.
+        Tests to see if the output of the count_combos method is a dataframe with a MultiIndex by calculating the numbers of levels in the index.
         '''
         dice = [1,2,3,4]
         dice_arr = np.array(dice)
@@ -238,7 +237,7 @@ class AnalyzerTestSuite(unittest.TestCase):
 
     def test_12_count_permutations(self):
         '''
-        This tests the the output of the count_permutations method is a dataframe with a MultiIndex.
+        Tests the the output of the count_permutations method is a dataframe with a MultiIndex.
         The MultiIndex is tested by counding the number of levels in the index.
         '''
 
