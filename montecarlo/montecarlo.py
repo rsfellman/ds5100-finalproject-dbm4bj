c90f08002e393d9c40568c76c6b68e4e4c27b914
Rachel Fellman <rachel.s.fellman@gmail.com>
2024-07-06 15:42:21 -0400

    fixed typos

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -6,7 +6,7 @@ import numpy as np
 class Die:
 
     '''
-    Create a weighted die with the option to roll it.
+    Create a die with the ability to change the weights of select faces of said die and roll the die, taking into account the weights.
     ---
     
     Methods:
@@ -14,25 +14,31 @@ class Die:
     __init__:   This is the initializer.
                 The user must input a numpy array with faced for the die.
     
-    change_weight: changes the weight of one face of the die.
+    change_weight: Changes the weight of one face of the die.
         
 
-    roll_die:   This rolls the die a given number of times.
+    roll_die:   Rolls the die a given number of times.
                 The results for roll_die will change as if the weights for the die are changed.
     
     
-    get_current_state: prints a data frame with the sides of the die as an index and the assigned weights for each side as the data.
+    get_current_state: Prints a data frame with the sides of the die as an index and the assigned weights for each side as the data.
+    
+    ---
+    Attributes: 
+    faces: NumPy array of the faces for a die.
  
     '''
 
     def __init__(self, N):
         '''
-        This is the initializer. It sets up the die with a given number of sides and adds a weight of 1 for each side of the die.
-        A private dataframe is also created. This dataframe has the sides of the die as an index and the weights as the data in a column.
+        Initializer. 
+        It sets up the die with a given number of sides and adds a weight of 1 for each side of the die.
+        A private dataframe is also created. 
+        The private dataframe has the sides of the die as an index and the weights as the data in a column.
 
         ---
         inputs:
-        N:  N is the number of faces for a die.  
+        N:  Faces for a die.  
             N must be a NumPy array with unique values. 
             This method will return errors if those conditions are not met.
         outputs:None
@@ -54,7 +60,7 @@ class Die:
 
     def change_weight(self, face, new_weight):
         '''
-        This changes the weight of one specified side of the die by assigning the new wieght to the data frame.
+        Changes the weight of one specified side of the die by assigning the new weight to the data frame.
         ---
         inputs:
         face:   The face from your die that you want to change the weight of.
@@ -76,11 +82,11 @@ class Die:
 
     def roll_dice(self, nrolls=1):
         '''
-        Roll_die calculates the probability for each side of the die from the assigned weights.
-        It then takes a sample of the sides using the calculated probabilities, and prints the results of the rolls as a list.
+        Calculates the probability for each side of the die from the assigned weights.
+        Next, takes a sample of the sides using the calculated probabilities, and prints the results of the rolls as a list.
         ---
         inputs:
-        nrolls: This is set to one unless the user reassigns it. 
+        nrolls: Default set to one unless the user reassigns it. 
                 Nrolls should be an integer.
         outputs:
         results: a python list of the results of the rolls
@@ -95,11 +101,11 @@ class Die:
     
     def get_current_state(self):
         '''
-        get_current_state prints a data frame with the sides of the die as an index and the assigned weights for each side as the data.
+        Prints a data frame with the sides of the die as an index and the assigned weights for each side as the data.
         ---
         inputs: none
         outputs:
-        die_df_index: a data frame with the faces of the die as an index and a single column with the assigned weights for each face.
+        die_df_index: data frame with the faces of the die as an index and a single column with the assigned weights for each face.
         '''
         
         return self.__die_df_index
@@ -109,40 +115,45 @@ class Die:
     
 class Game:
     '''
-    The Game class gives the user the ability to roll one or more similar die. The die should have the same number of sides and associated faces.
-    The game is initialized with a python list of one or more dice created from the die class.
+    Gives the user the ability to roll one or more similar die. 
+    The die should have the same number of sides and associated faces.
+    Game is initialized with a python list of one or more dice created from the die class.
     Game gives the user the ability to "roll" the inputed dice a given number of times.
     ---
     Methods:
-    __init__:   This is the initializer. It takes an input of a python list of dice.
+    __init__:   Initializer. It takes an input of a python list of dice.
 
     play:   The user calls this method to "roll" the die. The user is able to specify the number of rolls.
-            Play calculates the probability of each face based on the weights assigned to each side of the die.
+            Calculates the probability of each face based on the weights assigned to each side of the die.
             The die are sampled for the number of rolls specified and the output is stored in a private dataframe.
 
-    show_last_play:  The user calls this method to see the results of their most recent play.
+    show_last_play: Method to see the results of their most recent play.
                     A dataframe of the results is returned.
+    ---
+    attributes:
+    die_list: Python list of dice, where each element of the list is a single die.
     '''
     def __init__(self, die_list):
         '''
-        This is the initializer for the Game class
+        Initializer for the Game class
         ---
         inputs:
-        die_list:   die_list is a python list of dice created using the Die class.
+        die_list:   Python list of dice created using the Die class.
                     The die in die list should have the same number of sides and associated faces.
         outputs:
-        die_list: a python list of dice, where each element of the list is a single die.
+        die_list: Python list of dice, where each element of the list is a single die.
         '''
         self.die_list = die_list
     
     def play (self, rolls):
         '''
-        This method allows the user to "roll" the die. The probability of each face is calculated using the weights given in the die_list.
-        The play method samples the faces of the die for a given number of rolls based on the calculated probabilites and saves the result in a private dataframe.
+        Allows user to "roll" the die.
+        Probability of each face being rolled is calculated using the weights given in the die_list.
+        Samples the faces of the die for a given number of rolls based on the calculated probabilites and saves the result in a private dataframe.
         ---
         inputs:
-        rolls:  integer
-                rolls is an integer that specified how many time the die should be "rolled"/sampled.
+        rolls:  Integer
+                Specifies how many time the die should be "rolled"/sampled.
         outputs:none
         '''
         die_probs = list(range(len(self.die_list)))
@@ -170,14 +181,15 @@ class Game:
 
     def show_last_play (self, format = "wide"):
         ''' 
-        show_last_play is used to see the results of the most recent play. This method returns the private dataframe created by the play class.
-        The data can be returned in 'narrow' or 'wide' format and an error will be raised if a differnt format is supplied by the user.
+        Used to see the results of the most recent play. 
+        Returns the private dataframe created by the play class.
+        The data can be returned in 'narrow' or 'wide' format and an error will be raised if a different format is supplied by the user.
         ---
         inputs:
         format: "narrow" or "wide"
-                format defaults to wide but the user can choose to have the data presented in narrow format by entering "narrow".
+                Format defaults to wide but the user can choose to have the data presented in narrow format by entering "narrow".
         outputs:
-        last_play:  a dataframe of the results of the most recent play. 
+        last_play:  Dataframe of the results of the most recent play. 
                     Can be in either wide or narrow format.
         '''
         if format == "wide":
@@ -194,38 +206,41 @@ class Game:
 
 class Analyzer:
     ''' 
-    The Analyzer class takes the results of a game played via the Game class and computes statistics about said game.
+    Takes the results of a game played via the Game class and computes statistics about said game.
     ---
     methods:
-    __init__:   this is the initializer. It takes in an instance of the Game class.
+    __init__:   Initializer. It takes in an instance of the Game class.
                 If the input is not an instance of the Game class, a ValueError will be raised.
                 A dataframe named data is created. Data holds the information for the last play from the game class.
 
     count_jackpots: A jackpot is when all the faces for a given roll are the same.
                     The count_jackpots method counts the number of times a game resulted in a jackpot and returns an integer.
 
-    count_faces:    This calculates the number of times a given face is returned for a roll.
-                    A dataframe of the resutls is returned.
+    count_faces:    Calculates the number of times a given face is returned for a roll.
+                    A dataframe of the results is returned.
 
-    combo_count:    This counts the number of distinct combinations among the rolls.
+    combo_count:    Counts the number of distinct combinations among the rolls.
                     A distinct combination is order-independent and can included repetitions.
                     A data frame of the combinations and associated counts is returned.
 
-    permutation_count:  This method counts the number of disinct permutations among the rolls.
+    permutation_count:  Counts the number of disinct permutations among the rolls.
                         A distinct permutation is order-dependent and can include repetitions.
                         A data frame of the permutations and counts is returned.
+    ---
+    attributes:
+    data: a data frame of the last play.
 
     '''  
     def __init__(self, game):
         ''' 
-        This is the initializer. It takes in a an instance of the Game class. 
+        Initializer. Takes in a an instance of the Game class. 
         From the input of the game class the method show_last_play is called to create a data frame of the the last play.
         ---
         inputs:
-        game:   a Game object/instnace of the game class.
+        game:   A Game object/instnace of the game class.
                 If game is not a Game object, a ValueError will be raised.
         outputs: 
-        attributes: data: a data frame of the last play.
+        data: Data frame of the last play.
         '''
         if not isinstance(game, Game):
             raise ValueError("This is not a Game object. Please input a Game object.")
@@ -234,14 +249,14 @@ class Analyzer:
 
     def count_jackpots(self):
         ''' 
-        The count_jackpots method calculated the numbe rof times a game resulted in a jackpot and returns and integer.
+        Calculates the number of times a game resulted in a jackpot and returns and integer.
         A jackpot is when all the races for a given roll are the same.
         For each row of the data, this method finds the number of unique values. If the number of unique values is greater than 1, that roll was not a jackpot.
         For each row where the number of unique values is equal to one, the jackpot count goes up by one.
         ---
         inputs:none
         outputs: 
-        integer
+        jack_count: integer, the number of jackpots
         '''
         jack_count = 0
         for i in range(len(self.data)):
@@ -253,12 +268,12 @@ class Analyzer:
     
     def count_faces(self):
         ''' 
-        The count_faces method counts the number of times a given face appears in one roll using the value_counts() method with a lambda function.
+        Counts the number of times a given face appears in one roll using the value_counts() method with a lambda function.
         The lambda function is the applied across the data frame to calculate the face counts for each roll.
         ---
         inputs: none
         outputs: 
-        face_count: data frame with roll number as the index, faces as the columns and counts for each face appearance as the data.
+        face_count: Data frame with roll number as the index, faces as the columns and counts for each face appearance as the data.
         '''
         row_count = lambda row: row.value_counts()
         face_count = self.data.apply(row_count, axis = 1).fillna(0).astype(int)
@@ -271,7 +286,7 @@ class Analyzer:
         ---
         inputs: none
         outputs: 
-        combo_count_df: a dataframe with a MultiIndex of distinct combinations and a single column for the associated counts.
+        combo_count_df: Dataframe with a MultiIndex of distinct combinations and a single column for the associated counts.
         '''
         row_combos = self.data.apply(lambda row: tuple(sorted(row)), axis = 1)
         combo_count = row_combos.value_counts()
@@ -281,12 +296,12 @@ class Analyzer:
     
     def count_permutations(self):
         ''' 
-        count_permutations calculates the distinct permutations of faces rolled in a game and their counts.
+        Calculates the distinct permutations of faces rolled in a game and their counts.
         A permutaiton is dependent on order and may contain repetitions.
         ---
         inputs:none
         outputs:
-        perm_count_df: a datareame with a MultiIndex of distinct permutations and a column for the associated counts.
+        perm_count_df: Datareame with a MultiIndex of distinct permutations and a column for the associated counts.
         '''
         row_perm = self.data.apply(lambda row: tuple(row), axis = 1)
         perm_count = row_perm.value_counts()
